64b7b7886942df423e1ccfd5246e53ea64bf0f33
Ya'ar Hever <yaarhever@gmail.com>
2019-06-01 00:16:08 +0200

    Try to fix storybook build

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,24 @@
+const path = require("path");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = ({ config }) => {
+  config.resolve.extensions.push(".ts", ".tsx");
   config.module.rules.push({
-    test: /\.tsx?$/,
-    use: {
-      loader: "ts-loader",
-      options: {
-        transpileOnly: true
-      }
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve("babel-loader"),
+    options: {
+      presets: [["react-app", { flow: false, typescript: true }]]
     }
   });
-  config.resolve.extensions.push(".ts", ".tsx");
-  config.resolve.plugins = [new TsconfigPathsPlugin()];
+
+  config.resolve.plugins = config.resolve.plugins || [];
+  config.resolve.plugins.push(
+    new TsconfigPathsPlugin({
+      configFile: path.resolve(__dirname, "../tsconfig.json")
+    })
+  );
+  config.resolve.modules.push(path.resolve(__dirname, "../src"));
+
   config.devtool = "source-map";
   return config;
 };
